e6e0f964fbd127097c82ac75217f18847fe1f251
Jerome Velociter <jerome@velociter.fr>
2012-06-18 22:57:19 +0200

    Add server-side resource caching + handling of client-side resource caching

--- grails-app/controllers/org/mayocat/shop/grails/ResourceController.groovy
+++ grails-app/controllers/org/mayocat/shop/grails/ResourceController.groovy
@@ -1,56 +1,139 @@
 package org.mayocat.shop.grails
 
 import java.net.URI
-import org.apache.commons.io.IOUtils
+import java.net.URL;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang3.StringUtils
+
+import com.google.common.cache.CacheBuilderSpec
+import com.google.common.cache.CacheBuilder
+import com.google.common.cache.CacheLoader
+import com.google.common.cache.LoadingCache
+import com.google.common.io.Resources
+import com.google.common.net.HttpHeaders;
+import com.google.common.hash.Hashing
 
 class ResourceController {
 
-  static def SLASH = "/"
+    static scope = "singleton"
+    static def SLASH = "/"
+    static def STOREFRONTS_DIR = "storefronts"
+    
+    // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  static def STOREFRONTS_DIR = "storefronts"
+    private static class CachedResource {
+        def byte[] resource;
+        def String eTag;
+        def long lastModifiedTime;
 
-  def serve = {
-    def request = unwrapRequest(request)
-    def requestURI = request.forwardURI ?: request.requestURI
-    def contextPath = request.contextPath
-    def resourcePath = requestURI[requestURI.indexOf("/resources/")..-1]
-    def storefront = Shop.list()[0]?.storefront
-    if (!storefront || storefront == "") {
-      storefront = "default"
+        private CachedResource(final byte[] resource) {
+            this.resource = resource;
+            this.eTag = Hashing.md5().hashBytes(resource).toString();
+            this.lastModifiedTime = roundedTimestamp();
+        }
+
+        private long roundedTimestamp() {
+            return (System.currentTimeMillis() / 1000) * 1000;
+        }
     }
-    def filePath = this.getFilePath(resourcePath, storefront)
-    def is = servletContext.getResourceAsStream(filePath)
-    if (is == null) {
-      throw new java.io.FileNotFoundException("Could not find file with path ${filePath}")
+
+    private static class ResourceLoader extends CacheLoader<String, CachedResource> {
+        private final String resourcePath;
+        private final String uriPath;
+
+        private ResourceLoader(String resourcePath, String uriPath) {
+            this.resourcePath = resourcePath
+            this.uriPath = uriPath
+        }
+        
+        @Override
+        public CachedResource load(String key) throws Exception {
+            final String resource = key.substring(uriPath.length());
+            String fullResourcePath = this.resourcePath + resource;
+            final URL resourceURL = servletContext.getResource(fullResourcePath);
+            return new CachedResource(Resources.toByteArray(resourceURL));
+        }
     }
-    def filename = filePath[filePath.lastIndexOf("/") + 1..-1];
-    def mime = servletContext.getMimeType(filename.toLowerCase())
-    def data = IOUtils.toByteArray(is);
-    this.prepareHeaders(mime, data.length)
-    def out = response.outputStream
-    out.write(data);
-    out.close()
-  }
 
-  def getFilePath(String filepath, String storefront) {
-    def path = URI.create(SLASH + STOREFRONTS_DIR + SLASH + storefront + SLASH + filepath).normalize().toString()
-    if (!path.startsWith(SLASH + STOREFRONTS_DIR + SLASH + storefront + SLASH)) {
-      log.warn("Illegal access for file [{}]. Possible break-in attempt!", filepath)
+    // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+    private LoadingCache<String, CachedResource> cache;
+
+    // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+
+    /**
+     * Lazy cache initialization
+     * 
+     * @return the resource cache
+     */
+    def getCache(contextPath) {
+        if (this.cache == null) {
+            def shop = Shop.list()[0]
+            def storefront = shop?.storefront
+            if (!storefront || storefront == "") {
+                storefront = "default"
+            }
+            
+            this.cache = CacheBuilder.from(CacheBuilderSpec.parse("maximumSize=100"))
+                .build(new ResourceLoader(SLASH + STOREFRONTS_DIR + SLASH + storefront, contextPath))
+        }
+        this.cache
     }
-    path
-  }
+    
+    /**
+     * Try to serve to requested resource.
+     * 
+     * First, check if the client has a non-expired cached version of the resource
+     */
+    def serve = {
+        def request = unwrapRequest(request)
+        def requestURI = request.forwardURI ?: request.requestURI
+        def contextPath = request.contextPath ?: ""
 
-  def prepareHeaders(mimetype, length) {
-    if (!StringUtils.isBlank(mimetype)) {
-      response.setContentType(mimetype);
-    } else {
-      response.setContentType("application/octet-stream");
-    }    
-  }
+        def resource = this.getCache(contextPath).getUnchecked(requestURI)        
+        
+        if (this.isCachedByClient(request, resource)) {
+            response.sendError(HttpServletResponse.SC_NOT_MODIFIED);
+            return;
+        }
 
-  def unwrapRequest(request) {
-    request.request ?: request
-  }
+        def filename = requestURI[requestURI.lastIndexOf("/")..-1]
+        def mime = servletContext.getMimeType(filename.toLowerCase())
+
+        def data = resource.resource
+
+        response.setDateHeader(HttpHeaders.LAST_MODIFIED, resource.lastModifiedTime)
+        response.setHeader(HttpHeaders.ETAG, resource.eTag)
+        response.setContentType(mime ?: "application/octet-stream")
+
+        def out = response.outputStream
+        try {
+          out.write(data);
+        }
+        finally {
+          out.close()
+        }
+    }
+
+    /**
+     * Checks whether the request for the passed resource is cached on the client (browser), checking both 
+     * last modification date and etag headers.
+     *  
+     * @param request the request to check client caching for
+     * @param resource the server-side resource to check client caching for
+     * @return true if the client has not-expired cache resource, false otherwise
+     */
+    private boolean isCachedByClient(request, resource) {
+        return (resource.eTag == request.getHeader(HttpHeaders.IF_NONE_MATCH)) ||
+        (request.getDateHeader(HttpHeaders.IF_MODIFIED_SINCE) >= resource.lastModifiedTime)
+    }
+
+    def unwrapRequest(request) {
+        request.request ?: request
+    }
 
 }
