e845a852e609e0f5f318db4e8d01ed48680465d4
Jerome Velociter <jerome@velociter.fr>
2012-08-08 15:56:31 +0200

    Tests for the user resource + improvements

---
+++ test/src/test/java/org/mayocat/shop/rest/resources/UserResourceTest.java
@@ -0,0 +1,69 @@
+package org.mayocat.shop.rest.resources;
+
+import javax.ws.rs.core.MediaType;
+
+import junit.framework.Assert;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mayocat.shop.model.User;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.ClientResponse.Status;
+import com.yammer.dropwizard.validation.InvalidEntityException;
+
+public class UserResourceTest extends AbstractAuthenticatedResourceTest
+{
+    private UserResource userResource;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    @Override
+    protected void setUpResources() throws Exception
+    {
+        super.setUpResources();
+
+        this.userResource = this.componentManager.getInstance(Resource.class, "UserResource");
+        addResource(this.userResource);
+    }
+
+    @Test
+    public void testGetCurrentUserWhenAuthenticated() throws Exception
+    {
+        User user = client().resource("/user/_me")
+                            .header("Authorization", this.getBasicAuthenticationHeader())
+                            .get(User.class);
+
+        Assert.assertEquals(user.getEmail(), USER_NAME);
+        Assert.assertEquals(user.getPassword(), "********");
+
+    }
+
+    @Test
+    public void testGetCurrentUserWhenNotAuthenticated() throws Exception
+    {
+        ClientResponse cr = client().resource("/user/_me").get(ClientResponse.class);
+
+        Assert.assertEquals(Status.UNAUTHORIZED, cr.getClientResponseStatus());
+    }
+    
+    @Test
+    public void testRegisterUserValidation() throws Exception
+    {
+        // FIXME I don't know why this exception prevents from actually testing the response code.
+        thrown.expect(InvalidEntityException.class);
+        
+        ClientResponse cr = client().resource("/user/")
+                                    .type(MediaType.APPLICATION_JSON)
+                                    .entity("{\"email\":\"_me\", \"password\" : \"lol\"}")
+                                    .header("Authorization", this.getBasicAuthenticationHeader())
+                                    .put(ClientResponse.class);
+
+        
+        // Direct code comparison as 422 is a WebDAV extension which Jersey client has no mapping for.
+        // Assert.assertEquals(422, cr.getStatus());
+    }
+
+}
