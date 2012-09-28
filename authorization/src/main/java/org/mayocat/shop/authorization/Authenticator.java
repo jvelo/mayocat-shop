58be6e2a66959d6a7db1828d659708cd6589c357
Jerome Velociter <jerome@velociter.fr>
2012-09-28 12:46:25 +0200

    Documentation of some authorization interfaces

--- authorization/src/main/java/org/mayocat/shop/authorization/Authenticator.java
+++ authorization/src/main/java/org/mayocat/shop/authorization/Authenticator.java
@@ -1,14 +1,37 @@
 package org.mayocat.shop.authorization;
 
+import org.mayocat.shop.authorization.cookies.CookieAuthenticator;
 import org.mayocat.shop.model.User;
 import org.xwiki.component.annotation.Role;
 
 import com.google.common.base.Optional;
 
+/**
+ * An authenticator is capable of authentifying request based on the value of a certain header. Each authenticator only
+ * respond to certain types of header name/value pairs. For example, a {@link BasicAuthenticator} will only accept to
+ * respond to the authentication challenge when passed a header "Authorization" with a value that starts with "Basic".
+ * Similarly, a {@link CookieAuthenticator} will only accept to respond when passed a "Cookie" header and a value that
+ * contains a certain number of cookies it needs (encrypted username and password, etc). Implementation must specify
+ * which header name/value paris they accept to respond to in the {@link #respondTo(String, String)} method.
+ * Authentication verification logic should then be implemented in the {@link #verify(String)} method, where the value
+ * passed is always a header value the authenticator has accepted to respond to.
+ * 
+ * @version $Id$
+ */
 @Role
 public interface Authenticator
 {
+    /**
+     * @param headerName the name of the header this authenticator has to accept or refuse to respond to
+     * @param headerValue the value of the header this authenticator has to accept or refuse to respond to
+     * @return true if this authenticator accepts to respond to this challenge, false otherwise
+     */
     boolean respondTo(String headerName, String headerValue);
-    
+
+    /**
+     * @param value the header value to authenticate
+     * @return an empty option when the authenticator did not authentify any user against the challenge, an option with
+     *         the authenticated user otherwise.
+     */
     Optional<User> verify(String value);
 }
