7eda1fa5982f676608b9efc935e6c0c8df5eb501
Jerome Velociter <jerome@velociter.fr>
2012-10-05 15:31:04 +0200

    Remove unused import

--- store/datanucleus/store/src/main/java/org/mayocat/shop/store/datanucleus/DNCategoryStore.java
+++ store/datanucleus/store/src/main/java/org/mayocat/shop/store/datanucleus/DNCategoryStore.java
@@ -2,7 +2,6 @@ package org.mayocat.shop.store.datanucleus;
 
 import org.mayocat.shop.model.Category;
 import org.mayocat.shop.store.CategoryStore;
-import org.mayocat.shop.store.ProductStore;
 import org.xwiki.component.annotation.Component;
 
 @Component(hints = {"datanucleus", "default"})
