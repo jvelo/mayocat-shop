84352ba227b54c828523f3265ff08d312b272670
Jerome Velociter <jerome@velociter.fr>
2012-10-08 23:51:57 +0200

    New test

---
+++ store/datanucleus/store/src/test/java/org/mayocat/shop/store/datanucleus/DNCategoryStoreTest.java
@@ -0,0 +1,53 @@
+package org.mayocat.shop.store.datanucleus;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mayocat.shop.model.Category;
+import org.mayocat.shop.model.Product;
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.test.annotation.MockingRequirement;
+
+public class DNCategoryStoreTest extends AbstractStoreEntityTestCase
+{
+    @MockingRequirement(exceptions = {PersistenceManagerProvider.class})
+    private DNCategoryStore categoryStore;
+    
+    @Before
+    public void setUpRequirements() throws ComponentLookupException, Exception
+    {
+        super.setUpRequirements();
+    }
+    
+    @Test
+    public void testCreateAndUpdateCategory() throws Exception
+    {
+        Category category = new Category();
+        category.setHandle("my-category");
+        category.setTitle("My category");
+        
+        Product p1 = new Product();
+        p1.setTitle("P1");
+        p1.setHandle("p1");
+
+        Product p2 = new Product();
+        p2.setTitle("P2");
+        p2.setHandle("p2");
+        
+        category.addToProducts(p1);
+        category.addToProducts(p2);
+        
+        this.categoryStore.create(category);
+        
+        category = this.categoryStore.findByHandle("my-category");
+        
+        Assert.assertEquals(2, category.getProducts().size());
+        
+        Assert.assertEquals(p1, category.getProducts().get(0));
+        Assert.assertEquals(p2, category.getProducts().get(1));
+        
+        p1 = category.getProducts().get(0);
+        p2 = category.getProducts().get(1);
+    }
+}
+
