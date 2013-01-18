9f83d75d41a6eb2e24ff7611d61515416a280000
Jerome Velociter <jerome@velociter.fr>
2013-01-18 16:09:29 +0100

    Refactoring : extract a service out of the configuration controller

--- client/src/main/resources/assets/client/javascripts/configuration.js
+++ client/src/main/resources/assets/client/javascripts/configuration.js
@@ -1,113 +1,145 @@
 'use strict'
 
 angular.module('configuration', ['ngResource'])
-  .controller('ConfigurationController', ['$scope', '$resource',
+  .factory('configurationService', function($resource){
 
-      function($scope, $resource) {
+        var configurationResource = $resource("/configuration/", {}, {
+            update: {method: "PUT"}
+        });
+
+        var configuration = configurationResource.get(function(configuration){
+            saveOriginalValues();
+        });
+
+        function saveOriginalValues() {
+            function isConfigurable(node) {
+                return typeof node.configurable !== "undefined"
+                    && typeof node.value !== "undefined"
+                    && typeof node.default !== "undefined"
+                    && typeof node.visible !== "undefined";
+            }
+            function walk(node) {
+                for (var property in node) { if (node.hasOwnProperty(property)) {
+                    if (isConfigurable(node[property])) {
+                        node[property].__originalValue = node[property].value;
+                    }
+                    else if (typeof node[property] === "object") {
+                        // We need to go deeper...
+                        walk(node[property]);
+                    }
+                    else {
+                        // What do we do with properties that are not configurable ?
+                        // Nothing for now. We need to see if we want to support them first.
+                    }
+                }}
+            }
+            walk(configuration);
+        }
+
+        function prepareConfiguration(configuration) {
+            function isConfigurable(node) {
+                return typeof node.configurable !== "undefined"
+                    && typeof node.value !== "undefined"
+                    && typeof node.default !== "undefined"
+                    && typeof node.visible !== "undefined";
+            }
+            function isStillDefaultValue(node) {
+                if (node.value === node.__originalValue && node.value === node.defaultValue) {
+                    // Nothing changed
+                    return true;
+                }
+                return false;
+            }
+            function walk(node, container) {
+                for (var property in node) { if (node.hasOwnProperty(property)) {
+                    if (isConfigurable(node[property])) {
+                        if (!isStillDefaultValue(node[property])) {
+                            container[property] = node[property].value;
+                        }
+                    }
+                    else if (typeof node[property] === "object") {
+                        // We need to go deeper...
+                        container[property] =  walk(node[property], {});
+                    }
+                    else {
+                        // What do we do with properties that are not configurable ?
+                        // Nothing for now. We need to see if we want to support them first.
+                    }
+                }}
+                return container;
+            }
+            return walk(configuration, {});
+        };
+
+        return {
+            get:function (path) {
+                if (typeof path === "undefined") {
+                    return configuration;
+                }
+                var configurationElement = eval("configuration." + path);
+                if (typeof configurationElement === "undefined") {
+                    // The configuration does not exist
+                    return undefined;
+                }
+                return configurationElement.value;
+            },
+
+            put: function(config) {
+                configurationResource.update(prepareConfiguration(configuration));
+            },
+
+            isVisible:function (path) {
+                var configurationElement = eval("configuration." + path);
+                if (typeof configurationElement === "undefined") {
+                    // The configuration does not exist
+                    return false;
+                }
+                return typeof configurationElement.visible === "undefined"
+                    || configurationElement.visible;
+            },
+
+            isConfigurable:function (path) {
+                var configurationElement = eval("configuration." + path);
+                if (typeof configurationElement === "undefined") {
+                    // The configuration does not exist
+                    return false;
+                }
+                return typeof configurationElement.configurable === "undefined"
+                    || configurationElement.configurable;
+            },
+
+            isDefaultValue: function(path) {
+                var configurationElement = eval("configuration." + path);
+                if (typeof configurationElement === "undefined") {
+                    // The configuration does not exist
+                    return false;
+                }
+                return typeof configurationElement.default !== "undefined"
+                    && angular.equals(configurationElement.default, configurationElement.value);
+            }
+        };
+   })
+  .controller('ConfigurationController', ['$scope', 'configurationService',
+
+      function($scope, configurationService) {
 
         $scope.updateConfiguration = function() {
-          $scope.ConfigurationResource.update($scope.prepareConfiguration($scope.configuration));
+          configurationService.put($scope.configuration);
         };
 
         $scope.isVisible = function(path) {
-          var configurationElement = $scope.$eval("configuration." + path)
-          if (typeof configurationElement === "undefined") {
-            // The configuration does not exist
-            return false;
-          }
-          return typeof configurationElement.visible === "undefined"
-              || configurationElement.visible;
+            return configurationService.isVisible(path);
         }
 
         $scope.isConfigurable = function(path) {
-          var configurationElement = $scope.$eval("configuration." + path)
-          if (typeof configurationElement === "undefined") {
-            // The configuration does not exist
-            return false;
-          }
-          return typeof configurationElement.configurable === "undefined"
-              || configurationElement.configurable;
+            return configurationService.isConfigurable(path);
         }
 
         $scope.isDefaultValue = function(path) {
-          var configurationElement = $scope.$eval("configuration." + path)
-          if (typeof configurationElement === "undefined") {
-            // The configuration does not exist
-            return false;
-          }
-          return typeof configurationElement.default !== "undefined"
-              && angular.equals(configurationElement.default, configurationElement.value);
+            return configurationService.isDefaultValue(path);
         };
 
-        $scope.saveOriginalValues = function() {
-          function isConfigurable(node) {
-            return typeof node.configurable !== "undefined"
-                && typeof node.value !== "undefined"
-                && typeof node.default !== "undefined"
-                && typeof node.visible !== "undefined";
-          }
-          function walk(node) {
-            for (var property in node) { if (node.hasOwnProperty(property)) {
-              if (isConfigurable(node[property])) {
-                node[property].__originalValue = node[property].value;
-              }
-              else if (typeof node[property] === "object") {
-                // We need to go deeper...
-                walk(node[property]);
-              }
-              else {
-               // What do we do with properties that are not configurable ?
-               // Nothing for now. We need to see if we want to support them first.
-              }
-            }}
-          }
-          walk($scope.configuration);
-        }
-
-        $scope.prepareConfiguration = function() {
-          var configuration = {};
-          function isConfigurable(node) {
-            return typeof node.configurable !== "undefined"
-                && typeof node.value !== "undefined"
-                && typeof node.default !== "undefined"
-                && typeof node.visible !== "undefined";
-          }
-          function isStillDefaultValue(node) {
-            if (node.value === node.__originalValue && node.value === node.defaultValue) {
-              // Nothing changed
-              return true;
-            }
-            return false;
-          }
-          function walk(node, container) {
-            for (var property in node) { if (node.hasOwnProperty(property)) {
-              if (isConfigurable(node[property])) {
-                if (!isStillDefaultValue(node[property])) {
-                  container[property] = node[property].value;
-                }
-              }
-              else if (typeof node[property] === "object") {
-                // We need to go deeper...
-                container[property] =  walk(node[property], {});
-              }
-              else {
-               // What do we do with properties that are not configurable ?
-               // Nothing for now. We need to see if we want to support them first.
-              }
-            }}
-            return container;
-          }
-          configuration = walk($scope.configuration, configuration);
-          return configuration;
-        };
-
-        $scope.ConfigurationResource = $resource("/configuration/", {}, {
-          update: {method: "PUT"}
-        });
-
-        $scope.configuration = $scope.ConfigurationResource.get(function(configuration){
-          $scope.saveOriginalValues();
-        });
+        $scope.configuration = configurationService.get();
       }
 
   ]);
--- client/src/test/javascript/spec/configuration.js
+++ client/src/test/javascript/spec/configuration.js
@@ -1,57 +1,94 @@
-describe('Configuration', function() {
+describe('Configuration', function () {
 
-  var configurationController,
-      httpBackend;
+    describe('Service', function () {
+        var configurationService,
+            httpBackend;
 
-  beforeEach(module('mayocat'));
+        beforeEach(module('mayocat'));
 
-  beforeEach(inject(function($injector, $rootScope, $controller) {
-    httpBackend = $injector.get('$httpBackend');
-    httpBackend.when('GET', '/configuration').respond({
-      "module" : {
-        "propertySet" : {
-          "property" : {
-            "value": "Hello",
-            "default" : "Bonjour",
-            "configurable": false,
-            "visible" : true
-          }
-        }
-      }
+        beforeEach(inject(function ($injector) {
+            httpBackend = $injector.get('$httpBackend');
+            httpBackend.when('GET', '/configuration').respond({
+                "module":{
+                    "propertySet":{
+                        "property":{
+                            "value":"Hello",
+                            "default":"Bonjour",
+                            "configurable":false,
+                            "visible":true
+                        }
+                    }
+                }
+            });
+
+            configurationService = $injector.get('configurationService');
+        }));
+
+        it("Should be defined", function () {
+            expect(configurationService).toBeDefined();
+        });
+
+        it("Should offer access to configurations", function () {
+            httpBackend.flush();
+            expect(configurationService.get("module.propertySet.property")).toBe("Hello");
+        });
+
+        it("Should verify a configuration configurability and visibility", function () {
+            httpBackend.flush();
+            expect(configurationService.isConfigurable("module.propertySet.property")).toBe(false);
+            expect(configurationService.isVisible("module.propertySet.property")).toBe(true);
+        });
     });
 
-    configurationController = $rootScope.$new();
-    configurationController.$apply();
-    $controller('ConfigurationController', { $scope: configurationController });
-  }));
 
-  it("Should be defined", function() {
-    expect(configurationController).toBeDefined();
-  });
+    describe('Controller', function () {
+        var configurationController,
+            httpBackend;
 
-  it("Should save original values", function(){
-    httpBackend.flush();
-    expect(configurationController.configuration.module.propertySet.property.__originalValue).toBe("Hello");
-  });
+        beforeEach(module('mayocat'));
 
-  it("Should verify a configuration configurability and visibility", function(){
-    httpBackend.flush();
-    expect(configurationController.isConfigurable("module.propertySet.property")).toBe(false);
-    expect(configurationController.isVisible("module.propertySet.property")).toBe(true);
-  });
+        beforeEach(inject(function ($injector, $rootScope, $controller) {
+            httpBackend = $injector.get('$httpBackend');
+            httpBackend.when('GET', '/configuration').respond({
+                "module":{
+                    "propertySet":{
+                        "property":{
+                            "value":"Hello",
+                            "default":"Bonjour",
+                            "configurable":false,
+                            "visible":true
+                        }
+                    }
+                }
+            });
 
-  it("Should prepare configuration object for submit", function(){
-    httpBackend.flush();
-    configurationController.configuration.module.propertySet.property.value = "Salut";
-    var prepared = configurationController.prepareConfiguration();
-    expect(prepared.module.propertySet.property).toBe("Salut");
-  });
+            configurationController = $rootScope.$new();
+            configurationController.$apply();
+            $controller('ConfigurationController', { $scope:configurationController });
+        }));
 
-  it("Should prepare configuration object for submit when value is overriding but has not been changed", function(){
-    httpBackend.flush();
-    var prepared = configurationController.prepareConfiguration();
-    expect(prepared.module.propertySet.property).toBe("Hello");
-  });
+        it("Should be defined", function () {
+            expect(configurationController).toBeDefined();
+        });
+
+        it("Should save original values", function () {
+            httpBackend.flush();
+            expect(configurationController.configuration.module.propertySet.property.__originalValue).toBe("Hello");
+        });
+
+        it("Should prepare configuration object for submit", function () {
+            httpBackend.flush();
+            configurationController.configuration.module.propertySet.property.value = "Salut";
+            httpBackend.expectPUT("/configuration", /{"module":{"propertySet":{"property":"Salut"}}}/).respond(200);
+            configurationController.updateConfiguration();
+        });
+
+        it("Should prepare configuration object for submit when value is overriding but has not been changed", function () {
+            httpBackend.flush();
+            httpBackend.expectPUT("/configuration", /{"module":{"propertySet":{"property":"Hello"}}}/).respond(200);
+            configurationController.updateConfiguration();
+        });
+    });
 
 
 });
