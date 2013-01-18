23df4e1185cf55f2c39d11ffdf668793b32a48d9
Jerome Velociter <jerome@velociter.fr>
2013-01-18 17:39:39 +0100

    [minor] Formatting

--- client/src/main/resources/assets/client/javascripts/catalog.js
+++ client/src/main/resources/assets/client/javascripts/catalog.js
@@ -1,42 +1,42 @@
 'use strict'
 
 angular.module('catalog', [])
-  .service('catalogService', function($http){
-    return {
-      listProducts: function(callback) {
-        $http.get('/product/').success(function(data) {
-          callback && callback.call(this, data);
-        });
-      },
-      listCategories: function(callback) {
-        $http.get('/category/').success(function(data) {
-          callback && callback.call(this, data);
-        });
-      },
-      moveCategory: function(slug, targeg, position) {
-     },
-      moveProduct: function(slug, target, position){
-        $http.post('/category/_all/move',
-                   "product=" + slug + "&" + position + "=" + target,
-                   { "headers" : {'Content-Type': 'application/x-www-form-urlencoded'} })
-          .success(function(data) {
-          });
-      },
-      move: function(path, slug, target, position) {
-        $http.post(path + slug + "/move",
-                   position + "=" + target,
-                   { "headers" : {'Content-Type': 'application/x-www-form-urlencoded'} })
-             .success(function(data) {
-             })
-             .error(function(data, status) {
+    .service('catalogService', function ($http, configurationService) {
+        return {
+            listProducts:function (callback) {
+                $http.get('/product/').success(function (data) {
+                    callback && callback.call(this, data);
+                });
+            },
+            listCategories:function (callback) {
+                $http.get('/category/').success(function (data) {
+                    callback && callback.call(this, data);
+                });
+            },
+            moveCategory:function (slug, targeg, position) {
+            },
+            moveProduct:function (slug, target, position) {
+                $http.post('/category/_all/move',
+                    "product=" + slug + "&" + position + "=" + target,
+                    { "headers":{'Content-Type':'application/x-www-form-urlencoded'} })
+                    .success(function (data) {
+                    });
+            },
+            move:function (path, slug, target, position) {
+                $http.post(path + slug + "/move",
+                    position + "=" + target,
+                    { "headers":{'Content-Type':'application/x-www-form-urlencoded'} })
+                    .success(function (data) {
+                    })
+                    .error(function (data, status) {
 
-             });
-      }
+                    });
+            }
 
-    };
-  })
-  .controller('CatalogController', ['$scope', '$location', 'catalogService',
-      function($scope, $location, catalogService) {
+        };
+    })
+    .controller('CatalogController', ['$scope', '$location', 'catalogService',
+    function ($scope, $location, catalogService) {
 
         // List of products
         $scope.products = [];
@@ -44,30 +44,30 @@ angular.module('catalog', [])
         // A "move position" operation to perform. It is set by the 'sortable' directive when the list sort order changes.
         $scope.changeOperation = undefined;
 
-        $scope.setRoute = function(href) {
-          $location.url(href);
+        $scope.setRoute = function (href) {
+            $location.url(href);
         };
 
-        catalogService.listProducts(function(products) {
-          $scope.products = products;
+        catalogService.listProducts(function (products) {
+            $scope.products = products;
         });
 
-        catalogService.listCategories(function(categories) {
-          $scope.categories = categories;
+        catalogService.listCategories(function (categories) {
+            $scope.categories = categories;
         });
 
-        $scope.changePosition = function() {
-          if (typeof $scope.changeOperation === "undefined") {
-            return;
-          }
+        $scope.changePosition = function () {
+            if (typeof $scope.changeOperation === "undefined") {
+                return;
+            }
 
-          catalogService.move(
-              $location.path(),
-              $scope.changeOperation.handle,
-              $scope.changeOperation.target,
-              $scope.changeOperation.position
-          );
+            catalogService.move(
+                $location.path(),
+                $scope.changeOperation.handle,
+                $scope.changeOperation.target,
+                $scope.changeOperation.position
+            );
 
-          $scope.changeOperation = undefined;
+            $scope.changeOperation = undefined;
         };
-  }]);
+    }]);
