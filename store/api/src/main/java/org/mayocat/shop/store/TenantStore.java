8723c2d589d6a65ba89755c24baf0858082d28c8
Jerome Velociter <jerome@velociter.fr>
2013-01-26 23:34:40 +0100

    Maven cleanup

--- store/api/src/main/java/org/mayocat/shop/store/TenantStore.java
+++ store/api/src/main/java/org/mayocat/shop/store/TenantStore.java
@@ -2,13 +2,12 @@ package org.mayocat.shop.store;
 
 import org.mayocat.shop.model.Tenant;
 import org.mayocat.shop.model.TenantConfiguration;
-import org.skife.jdbi.v2.sqlobject.Bind;
 import org.xwiki.component.annotation.Role;
 
 @Role
 public interface TenantStore extends Store<Tenant, Long>
 {
-    Tenant findBySlug(@Bind("slug") String slug);
+    Tenant findBySlug(String slug);
 
     void updateConfiguration(TenantConfiguration configuration);
 }
