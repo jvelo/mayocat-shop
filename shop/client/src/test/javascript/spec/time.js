52bb4416c7ddafb51605b12145a92a117bf30f29
Vincent Velociter <vincent.velociter@gmail.com>
2013-04-09 16:03:11 +0200

    Remove unused tests

--- shop/client/src/test/javascript/spec/time.js
+++ shop/client/src/test/javascript/spec/time.js
@@ -33,25 +33,6 @@ describe('Time', function () {
             expect(timestampAsDateFilter(undefined)).toBe(undefined);
         });
 
-        it("Sould convert timestamps to dates", function() {
-            // FIXME
-            // Ignore until it accounts for timezone
-            // expect(timestampAsDateFilter("1364488887772")).toBe("2013-03-28 17:41");
-        });
-
-        it("Sould accept timestamps as numbers", function() {
-            // FIXME
-            // Ignore until it accounts for timezone
-            //expect(timestampAsDateFilter(1364488887772)).toBe("2013-03-28 17:41");
-        });
-
-        it("Sould support date formats as arugment", function() {
-            // FIXME
-            // Ignore until it accounts for timezone
-            //expect(timestampAsDateFilter("1364488887772", "LLL")).toBe("March 28 2013 5:41 PM");
-        });
-
-
     });
 
     describe('Full ISO 8691 date string to local date filter', function () {
