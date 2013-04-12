16b9f5c211cb726ef6accfdde26cd030eeba9889
Vincent Velociter <vincent.velociter@gmail.com>
2013-04-12 15:13:13 +0200

    Add json tzdata and java tools to generate it

---
+++ platform/base/src/main/java/org/mayocat/tools/TimeZoneListJson.java
@@ -0,0 +1,174 @@
+package org.mayocat.tools;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.SortedMap;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+
+/**
+ * Prints out time zones and their standard offset in a JSON object, sorted by region
+ * and then by offset (all redondant tz are removed).
+ *
+ * Some code borrowed from org.joda.example.time.TimeZoneTable
+ * https://github.com/JodaOrg/joda-time/blob/master/src/example/org/joda/example/time/TimeZoneTable.java
+ */
+public class TimeZoneListJson {
+    static final long cNow = System.currentTimeMillis();
+
+    static final DateTimeFormatter cOffsetFormatter = new DateTimeFormatterBuilder()
+        .appendTimeZoneOffset(null, true, 2, 4)
+        .toFormatter();
+
+    public static void main(String[] args) throws Exception {
+        Set idSet = DateTimeZone.getAvailableIDs();
+        SortedMap<String, ArrayList<ZoneData>> zones = new TreeMap<String, ArrayList<ZoneData>>();
+
+        {
+            Iterator it = idSet.iterator();
+            int i = 0;
+            while (it.hasNext()) {
+                String id = (String) it.next();
+                // get only standard format (not stuff like Etc/GMT-xx, PST, etc.)
+                if (id.matches("[A-Za-z_-]+?/[A-Za-z_-]+?") && !id.matches("^Etc/[A-Za-z0-9_-]+?")) {
+                    String k = id.split("/")[0];
+                    if (zones.containsKey(k)) {
+                        zones.get(k).add(new ZoneData(id, DateTimeZone.forID(id)));
+                    } else {
+                        ArrayList<ZoneData> regionZones = new ArrayList<ZoneData>();
+                        regionZones.add(new ZoneData(id, DateTimeZone.forID(id)));
+                        zones.put(k, regionZones);
+                    }
+                }
+            }
+        }
+
+        PrintStream out = System.out;
+
+        out.println("{");
+
+        for (Map.Entry<String, ArrayList<ZoneData>> entry : zones.entrySet())
+        {
+            // skip 'empty' regions where no canonical zone is present
+            boolean hasCanonicalZone = false;
+            for (ZoneData z : entry.getValue())
+            {
+                if (z.isCanonical()) {
+                    hasCanonicalZone = true;
+                    break;
+                }
+            }
+            if (!hasCanonicalZone) continue;
+
+            // sort by std offset
+            Collections.sort(entry.getValue());
+
+            out.println(entry.getKey() + ": {");
+
+            for (ZoneData z : entry.getValue())
+            {
+                if (z.isCanonical()) {
+                    printRow(out, z);
+                }
+            }
+
+            out.println("},");
+        }
+
+        out.println("};");
+    }
+
+    private static void printRow(PrintStream out, ZoneData zone) {
+        out.print(quote(zone.getCanonicalID()) + ":" + " { stdOffset: " + quote(zone.getStandardOffsetStr()) + ", city: " + quote(zone.getCityName()));
+
+        out.println(" },");
+
+    }
+
+    private static String quote(String str) {
+        return '"' + str + '"';
+    }
+
+    private static class ZoneData implements Comparable {
+        private final String iID;
+        private final DateTimeZone iZone;
+
+        ZoneData(String id, DateTimeZone zone) {
+            iID = id;
+            iZone = zone;
+        }
+
+        public String getID() {
+            return iID;
+        }
+
+        public String getCanonicalID() {
+            return iZone.getID();
+        }
+
+        public boolean isCanonical() {
+            return getID().equals(getCanonicalID());
+        }
+
+        public String getCityName() {
+            String name;
+            String[] parts = getID().split("/");
+            if (parts.length > 2) {
+                name = parts[1] + " - " + parts[2];
+            } else {
+                name = parts[1];
+            }
+            return name.replace("_", " ");
+        }
+
+        public String getStandardOffsetStr() {
+            long millis = cNow;
+            while (iZone.getOffset(millis) != iZone.getStandardOffset(millis)) {
+                long next = iZone.nextTransition(millis);
+                if (next == millis) {
+                    break;
+                }
+                millis = next;
+            }
+            return cOffsetFormatter.withZone(iZone).print(millis);
+        }
+
+        public int compareTo(Object obj) {
+            ZoneData other = (ZoneData) obj;
+
+            int offsetA = iZone.getStandardOffset(cNow);
+            int offsetB = other.iZone.getStandardOffset(cNow);
+
+            if (offsetA < offsetB) {
+                return -1;
+            }
+            if (offsetA > offsetB) {
+                return 1;
+            }
+
+            int result = getCanonicalID().compareTo(other.getCanonicalID());
+
+            if (result != 0) {
+                return result;
+            }
+
+            if (isCanonical()) {
+                if (!other.isCanonical()) {
+                    return -1;
+                }
+            } else if (other.isCanonical()) {
+                return 1;
+            }
+
+            return getID().compareTo(other.getID());
+        }
+    }
+}
