995dff56ce4609ecef38a6c27a6b154e69f8e018
Jerome Velociter <jerome@velociter.fr>
2013-11-22 17:17:09 +0100

    Attachment memory store

--- platform/store/memory/src/main/java/org/mayocat/store/memory/BaseEntityMemoryStore.java
+++ platform/store/memory/src/main/java/org/mayocat/store/memory/BaseEntityMemoryStore.java
@@ -1,5 +1,6 @@
 package org.mayocat.store.memory;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -7,7 +8,10 @@ import java.util.UUID;
 import javax.annotation.Nullable;
 import javax.validation.Valid;
 
+import org.mayocat.model.Child;
+import org.mayocat.model.Entity;
 import org.mayocat.model.Identifiable;
+import org.mayocat.model.Slug;
 import org.mayocat.store.EntityAlreadyExistsException;
 import org.mayocat.store.EntityDoesNotExistException;
 import org.mayocat.store.InvalidEntityException;
@@ -22,7 +26,52 @@ import com.google.common.collect.Maps;
  */
 public class BaseEntityMemoryStore<T extends Identifiable> implements Store<T, UUID>
 {
-    Map<UUID, T> entities = Maps.newConcurrentMap();
+    protected Map<UUID, T> entities = Maps.newConcurrentMap();
+
+    protected Predicate<T> withSlug(final String slug)
+    {
+        return new Predicate<T>()
+        {
+            @Override public boolean apply(@Nullable T input)
+            {
+                if (!Slug.class.isAssignableFrom(input.getClass())) {
+                    throw new RuntimeException("Cannot apply by slug filter to non-slug entity");
+                }
+
+                return ((Slug) input).getSlug().equals(slug);
+            }
+        };
+    }
+
+    protected Predicate<T> withParent(final Entity parent)
+    {
+        return new Predicate<T>()
+        {
+            @Override public boolean apply(@Nullable T input)
+            {
+                if (!Child.class.isAssignableFrom(input.getClass())) {
+                    throw new RuntimeException("Cannot apply with parent filter to non-child");
+                }
+
+                return parent.getId().equals(((Child) input).getParentId());
+            }
+        };
+    }
+
+    protected Predicate<T> withParentId(final UUID... parentId)
+    {
+        return new Predicate<T>()
+        {
+            @Override public boolean apply(@Nullable T input)
+            {
+                if (!Child.class.isAssignableFrom(input.getClass())) {
+                    throw new RuntimeException("Cannot apply with parent filter to non-child");
+                }
+
+                return Arrays.asList(parentId).contains(((Child) input).getParentId());
+            }
+        };
+    }
 
     @Override
     public T create(@Valid T entity) throws EntityAlreadyExistsException, InvalidEntityException
