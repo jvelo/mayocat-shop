366d74d4dd53b1c47b3c4d012c33b8b24882bcf6
Jérôme Velociter <jerome@velociter.fr>
2014-07-22 17:32:47 +0200

    Issue #190 NPE on home page when deleting a product that is in the home featured product list

--- shop/catalog/src/main/groovy/org/mayocat/shop/catalog/api/v1/HomePageApi.groovy
+++ shop/catalog/src/main/groovy/org/mayocat/shop/catalog/api/v1/HomePageApi.groovy
@@ -79,14 +79,16 @@ class HomePageApi implements Resource
             List<ProductApiObject> featuredProducts = []
             lists.first().entities.each({ UUID id ->
                 def product = products.find({ Product product -> product.id == id})
-                def ProductApiObject featuredProduct = new ProductApiObject()
-                featuredProduct.withProduct(product)
-                def featuredImage = images.find({ Image image -> image.attachment.id == product.featuredImageId })
+                if (product != null) {
+                    def ProductApiObject featuredProduct = new ProductApiObject()
+                    featuredProduct.withProduct(product)
+                    def featuredImage = images.find({ Image image -> image.attachment.id == product.featuredImageId })
 
-                if (featuredImage) {
-                    featuredProduct.withEmbeddedFeaturedImage(featuredImage)
+                    if (featuredImage) {
+                        featuredProduct.withEmbeddedFeaturedImage(featuredImage)
+                    }
+                    featuredProducts << featuredProduct
                 }
-                featuredProducts << featuredProduct
             })
 
             homePageApiObject.featuredProducts = featuredProducts
