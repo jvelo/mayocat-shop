6461590bd11676cdb1d7c610f5cb768b4f8e9e57
Jérôme Velociter <jerome@velociter.fr>
2014-08-19 11:55:22 +0200

    Make scripts globally more reuse-friendly

--- shop/client/src/main/resources/assets/client/localization/en.js
+++ shop/client/src/main/resources/assets/client/localization/en.js
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-(function (Mayocat) {
+var Mayocat = (function (Mayocat) {
     'use strict';
 
     var enLocalization = {
@@ -495,4 +495,6 @@
     // Expose the localization to the Mayocat settings
     Mayocat.localization.en = enLocalization;
 
-})(Mayocat);
+    return Mayocat;
+
+})(Mayocat || {});
